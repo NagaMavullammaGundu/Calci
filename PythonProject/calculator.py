f5556fa38b31af4f7cd0eda73a329e25183134ed
2503-00016 <gundunm16@gmail.com>
2025-04-22 12:59:35 +0530

    Basic operations Implemented

--- PythonProject/calculator.py
+++ PythonProject/calculator.py
@@ -1,4 +1,36 @@
-def add(num1,num2):
-    return num1 + num2
+def operations(sign ,num1 = 0,num2 = 0):
+    if sign == '+':
+        result = num1 + num2
+        return result
+    elif sign == '-':
+        result = num1 - num2
+        return result
+    elif sign == '*':
+        result = num1 * num2
+        return result
+    elif sign == '%': #remainder
+        result = num1 % num2
+        return result
+    elif sign == '/': #Quotient
+        result = num1 / num2
+        return result
+    else:
+        result = num1
+        return result
 
-print(add(10,10))
+
+signslist = ['+','-','*','%','/']
+num1 = int(input("Enter number:"))
+sign = input("Enter sign :")
+
+if sign in signslist:
+    while sign in signslist:
+        num2 = int(input("Enter number:"))
+        updated_result = (operations(sign, num1, num2))
+        num1 = updated_result
+        sign = input("Enter sign :")
+        if sign == '=':
+            print(updated_result)
+
+
+
